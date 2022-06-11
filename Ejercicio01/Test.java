8de9429091db0442eebf97b1e76adeed22ca89d4
fluchi <fcardenasm@unsa.edu.pe>
2022-06-11 18:51:27 -0500

    agrego implementacion de  insercionSort generico

---
+++ Ejercicio01/Test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+    }
+
+    public static <E extends Comparable<E>> void insertionSort(LinkedList<E> list) {
+        E key;
+        int i;
+        for (int j = 1; j < list.tamanio; j = j + 1) {
+            key = list.get(j).getDato();
+            i = j - 1;
+            while (i > -1 && list.get(i).getDato().compareTo(key) > 0) {
+                list.get(i + 1).setDato(list.get(i).getDato());
+                i = i - 1;
+            }
+            list.get(i + 1).setDato(key);
+        }
+        System.out.println(list);
+    }
+
+}
