dad8f84977cdccabe640e9de8516aaf9bbae5c29
bryanhancco <93294188+bryanhancco@users.noreply.github.com>
2022-06-12 05:49:55 -0500

    Agregando una plantilla inicial

---
+++ Ejercicio02/DoubleLinkedList.java
@@ -0,0 +1,52 @@
+public class DoubleLinkedList<E extends Comparable<E>> implements TDAList<E> {
+    Node<E> first;
+    int tamanio;
+
+    public DoubleLinkedList() {
+
+    }
+    
+    @Override
+    public boolean isEmpty() {
+    	return false;
+    }
+
+    @Override
+    public void insertFirst(E x) {
+    	
+    }
+
+    @Override
+    public void insertLast(E x) {
+    	
+    }
+
+    @Override
+    public boolean search(E x) {
+    	return false;
+    }
+
+    @Override
+    public void remove(E x) {
+    	
+    }
+
+    @Override
+    public void insert(E x, int p) {
+    	
+    }
+
+    public void remove(int ind) {
+
+    }
+
+    public Node<E> get(int ind) {
+    	return null;
+    }
+
+    @Override
+    public String toString() {
+    	return "";
+    }
+
+}
