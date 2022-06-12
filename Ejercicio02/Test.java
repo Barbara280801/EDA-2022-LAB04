a7f9bf6ddd7c21712298bbe3fb0243bb200a7993
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-06-12 15:22:26 -0500

    test base

---
+++ Ejercicio02/Test.java
@@ -0,0 +1,11 @@
+import java.util.concurrent.*;
+import java.util.*;
+import com.panayotis.gnuplot.*;
+import java.io.*;
+
+public class Test {
+	public static void main (String[] args) throws Exception {
+		final int CANTIDAD = 10000;
+		long simulacionInicio, simulacionFin;
+	}
+}
