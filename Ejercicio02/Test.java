a2144173f7340e3ac00fee824f2c4ec5ae6c9b12
Carlo Rodrigo Diaz Portilla <cdiazpor@unsa.edu.pe>
2022-06-12 15:28:03 -0500

    Agrego el algoritmo de insercion

--- Ejercicio02/Test.java
+++ Ejercicio02/Test.java
@@ -8,4 +8,18 @@ public class Test {
 		final int CANTIDAD = 10000;
 		long simulacionInicio, simulacionFin;
 	}
+	public static <E extends Comparable<E>> long insertionSort (DoubleLinkedList<E> list){
+		long nano_startTime = System.nanoTime();
+		for (Node<E> key = list.get(0).getNext(); key != null; key = key.getNext()){
+			Node<E> i = key.getPrevious();
+			E aux = key.getData();
+			while(i != null && i.getData().compareTo(i.getNext().getData()) == 1){
+				i.getNext().setData(i.getData());
+				i.setData(aux);
+				i = i.getPrevious();
+			}
+		}
+		long nano_endTime = System.nanoTime();
+		return nano_endTime - nano_startTime;
+	}
 }
